cf62cd73d1c063d0dc129e8e842f9e99750fb787
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-10-11 17:26:42 +0300

    Basic extensions library
    Add stubs for missing classes

---
+++ component/backend/src/Library/Extension/Language.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * @package   onthos
+ * @copyright Copyright (c) 2024 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Component\Onthos\Administrator\Library\Extension;
+
+use SimpleXMLElement;
+
+defined('_JEXEC') || die;
+
+class Language extends Extension
+{
+	protected function populateExtensionImportantPaths(): void
+	{
+		// TODO: Implement populateExtensionImportantPaths() method.
+	}
+
+	protected function populateDefaultLanguageFiles(): void
+	{
+		// TODO: Implement populateDefaultLanguageFiles() method.
+	}
+
+	protected function addLanguagesFromManifest(SimpleXMLElement $xml): void
+	{
+		// TODO: Implement addLanguagesFromManifest() method.
+	}
+
+	protected function getScriptPathFromManifest(SimpleXMLElement $xml)
+	{
+		// TODO: Implement getScriptPathFromManifest() method.
+	}
+}
